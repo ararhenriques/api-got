c49d8e888c1d87c3731029f8ff7933d53314da14
Arrhe <arrhenr@gmail.com>
2019-01-23 15:26:33 -0500

    1/23/19 v2

--- gotapi.js
+++ gotapi.js
@@ -4,55 +4,21 @@ let url;
 // search form
 const searchInput = document.getElementById("option"); 
 const searchButton = document.getElementById("submit");
+//results
+const section = document.getElementById("results");
+const title = document.getElementById("name")
+const region = document.getElementById("region")
+const coatOfArms = document.getElementById("coatOfArms")
+const words = document.getElementById("words")
 
-/* //results nav
-const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.prev');
-const nav = document.querySelector('nav');
 
-//results section
-const section = document.querySelector('section');
-
-nav.style.display = 'none';
-
-let pageNumber = 1; 
-
-let displayNav = false; */
-
-/* nextBtn.addEventListener('click', nextPage);
-previousBtn.addEventListener('click', previousPage); */
-
-//url
-// function makeOptions(event) {
-//     event.preventDefault()
-//     console.log()
-//     fetch(baseURL)
-//     .then(function(result){
-//         console.log(result)
-//         return result.json();
-//     })
-//     .then(function(json) {
-//         console.log(json)
-//         displayResults(json);
-//     })
-    
-//     json.forEach(option => {
-//         let elem = document.createElement('option');
-//         elem.innerText=[].name;
-//         Selection.appendChild(elem);
-//     })
-// }
-
-// function makeOptions(json) {
-//      console.log(json);
-// }
 searchButton.addEventListener('click', fetchResults);
 
 function fetchResults(e) { //e allows us to interact with the object that we're fetching (placeholder?)
 e.preventDefault()  
 console.log(searchInput.value)
 // console.log(e);
-  url = `${baseURL}/${searchInput.value}`; //assembling the full url
+  url = `${baseURL}${searchInput.value}`; //assembling the full url
   console.log('URL:', url);
   //e.preventDefault();
 
@@ -63,12 +29,21 @@ fetch(url) // Info request w/ url as a parameter
       return result.json(); // returns the request, but not easily interpretable
     })
     .then(function(json) {
+        console.log(json);
       console.log(json);
-      console.log(json.coatOfArms);
+      console.log(json.name);
       console.log(json.region);
+      console.log(json.coatOfArms);
       console.log(json.words);
       displayResults(json); //returning the results in a way we can read
     })
 }
-/* console.log(json.value)
-function displayResults(json) {} */
+function displayResults(json){
+  title.innerText = "Name: "+json.name
+  region.innerText = "Region: " +json.region
+  coatOfArms.innerText = "Coat of Arms: " +json.coatOfArms
+  words.innerText = "Words: " +json.words
+}
+
+
+
